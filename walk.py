dae51e39d7d4e42312d5f5f88c7e9e52fe67883f
iTofunmi <114885123+iTofunmi@users.noreply.github.com>
2024-02-06 08:10:56 +0100

    Update walk.py

--- walk.py
+++ walk.py
@@ -3,6 +3,12 @@ from os.path import isdir, join
 
 def walk_directory(d, indent):
     # Implement recursive directory walk here
+    merged_directory = join(d,f)
+    for f in listdir(d):
+        print(indent + f)
+        if isdir(merged_directory):#if it is true
+            walk_directory(merged_directory, 2 * indent)
+        
 
 # '.' means current directory
 walk_directory('Desktop', ' ')
